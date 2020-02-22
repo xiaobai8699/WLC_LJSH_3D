223ce3b901a2143e8fcbc01729488a840d81432b
李红 <lh.work@qq.com>
2020-02-22 18:51:44 +0800

    Create Loader.ts

---
+++ src/core/Loader.ts
@@ -0,0 +1,52 @@
+
+import * as THREE from 'three';
+import { GLTFLoader, GLTF } from 'three/examples/jsm/loaders/GLTFLoader';
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
+import { ResourceTracker } from '../asset/ResourceTracker';
+
+export class Loader {
+
+    resourceTrackerMap: Map<string, any> = new Map();
+
+    load = (name: string, decompress: boolean = true, progress:any,success:Function, error:any ) => {
+
+        const loader = new GLTFLoader();
+
+        if (decompress) {
+
+            const dracoLoader = new DRACOLoader().setDecoderPath('./core/draco/');
+            loader.setDRACOLoader(dracoLoader);
+
+        }
+
+        loader.load(
+
+            name,
+
+            (gltf: GLTF) => {
+
+                const rt = new ResourceTracker();
+                rt.track(gltf.scene);
+                this.resourceTrackerMap.set(name, rt);
+
+                success(gltf);
+            },
+
+            progress, // p means progress
+
+            error  // e means error
+        );
+
+    }
+
+    unload = (name: string) => {
+
+        const rt: ResourceTracker = this.resourceTrackerMap.get(name);
+        if (rt) {
+            rt.dispose();
+            this.resourceTrackerMap.delete(name);
+        }
+
+    }
+
+}

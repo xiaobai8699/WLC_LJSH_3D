21d7cc89f2488b35721647c6dfa43f9a9382ba12
李红 <lh.work@qq.com>
2020-02-25 16:28:50 +0800

    Create FPSControls.ts

---
+++ src/components/FPSControls/FPSControls.ts
@@ -0,0 +1,51 @@
+
+import {
+    Scene,
+    Camera,
+    WebGLRenderer,
+    Vector3,
+    Object3D,
+    Euler
+}from 'three';
+import {RotateControls} from './RotateControls';
+import {MoveControls} from './MoveControls';
+
+export class FPSControls {
+
+    private rotateCameraControls: RotateControls;
+
+    private rotateHeroControls: RotateControls;
+
+    private moveControls: MoveControls;
+
+    constructor (scene: Scene, camera: Camera, renderer: WebGLRenderer) {
+
+        this.moveControls = new MoveControls(scene);
+
+        this.rotateHeroControls = new RotateControls(this.moveControls.target, renderer.domElement);
+        this.rotateHeroControls.disableXAxis = true;
+
+        this.rotateCameraControls = new RotateControls(camera, renderer.domElement);
+        this.rotateCameraControls.disableYAxis = true;
+
+        this.moveControls.target.add(camera);
+
+        this.moveControls.target.position.set(2741, 1, -1310);
+        camera.position.set(0,2.5,12);
+
+    }
+
+    
+    public update = (delta: number) => {
+
+        this.moveControls.update(delta);
+        this.rotateCameraControls.update(delta);
+
+    }
+
+    public dispose = () => {
+
+        this.rotateCameraControls.dispose();
+        this.moveControls.dispose();
+    }
+}

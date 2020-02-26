14a9eded2736a58f5b4e68c643644a58f0ff7cc7
李红 <lh.work@qq.com>
2020-02-26 20:32:54 +0800

    Update MoveControls.ts

--- src/components/FPSControls/MoveControls.ts
+++ src/components/FPSControls/MoveControls.ts
@@ -18,9 +18,7 @@ export class MoveControls {
         this.keyboardState = new KeyboardState();
 
         if(!target) {
-            const mat = new MeshStandardMaterial({color:0xFF0000});
-            const geo = new BoxGeometry(1,1,1);
-            this.target = new Mesh(geo, mat);
+            this.target = new Object3D();
             scene.add(this.target);
         }
     }
@@ -29,11 +27,11 @@ export class MoveControls {
 
         var speed = delta * 20;
 
-        if (this.keyboardState.pressed(KeyboardState.LEFT)) this.target.translateX(- speed);
+        if (this.keyboardState.pressed(KeyboardState.LEFT)) this.target.translateX(-speed);
 
         if (this.keyboardState.pressed(KeyboardState.RIGHT)) this.target.translateX(speed);
 
-        if (this.keyboardState.pressed(KeyboardState.FRONT)) this.target.translateZ(- speed);
+        if (this.keyboardState.pressed(KeyboardState.FRONT)) this.target.translateZ(-speed);
 
         if (this.keyboardState.pressed(KeyboardState.BACK))  this.target.translateZ(speed);
 

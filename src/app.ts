d721d3ca27821b9354a4f8e85b6fad02f643f3cb
李红 <lh.work@qq.com>
2020-03-02 22:14:09 +0800

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -16,12 +16,16 @@ import {
     CanvasTexture,
     Texture,
     DataTexture,
-    RGBAFormat
+    RGBAFormat,
+    Vector2
 } from 'three';
 import { FPSControls } from './js/components/FPSControls/FPSControls'
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 import { Core } from './js/common/Core';
 import { Loader } from './js/common/Loader';
 import { GLTF } from 'three/examples/jsm/loaders/GLTFLoader';
+import {TreeTextureManager} from './js/common/TreeTextureManager';
+import {Tree} from './js/common/Tree';
 import * as Stats from 'stats.js';
 
 export class App {
@@ -32,69 +36,38 @@ export class App {
 
     stats: Stats = new Stats();
 
-    fpsControl: FPSControls = new FPSControls(this.core.scene, this.core.camera, this.core.renderer);
+    // fpsControl: FPSControls = new FPSControls(this.core.scene, this.core.camera, this.core.renderer);
+
+    orbitControls: OrbitControls = new OrbitControls(this.core.camera, this.core.renderer.domElement);
 
     clock: Clock = new Clock();
 
     texture: Texture;
 
-    start = () => {
+    tree: Tree = null;
+
+    constructor(){
+
+        this.tree = new Tree(this.core.scene,this.core.camera);
+
+    }
+
+    async start() {
 
         document.body.appendChild(this.stats.dom);
 
-
         this.loadModel();
+        this.tree.create();
 
-
-        const scene = this.core.scene;
-        const self = this;
-
-        function makeSpriteCanvas(imageName: string) {
-
-            const loader = new ImageLoader();
-            loader.load(`./asset/texture/plant/${imageName}.png`, image => {
-
-                const size = 512 / 8;
-
-                const ctx: CanvasRenderingContext2D = document.createElement('canvas').getContext('2d');
-                ctx.canvas.width = size;
-                ctx.canvas.height = size;
-                ctx.drawImage(image, 0, 6*size, size, size, 0, 0, size, size);
-
-                self.texture = new DataTexture(ctx.getImageData(0, 0, size, size).data, size, size);
-
-                const mat = new SpriteMaterial({ map: self.texture, color: 0xffffff });
-                    const sprite = new Sprite(mat);
-                    sprite.position.set(0,1,0);
-                    //  sprite.scale.set(1, 1, 0);
-                    scene.add(sprite);
-
-                let now = Date.now();
-                const count = 100;
-                for(let x = -count/2; x < count/2; x++){
-                    for(let y =0 ; y< count; y++) {
-                        const mat = new SpriteMaterial({ map: self.texture, color: 0xffffff });
-                    const sprite = new Sprite(mat);
-                    sprite.position.set(x+2741 , y ,-1310);
-                    //  sprite.scale.set(1, 1, 0);
-                    scene.add(sprite);
-                    }
-                }
-                console.log(`Time:${Date.now()-now}`);
-
-            });
-
-        }
-
-        makeSpriteCanvas('tree1_mobile');
-        this.core.renderer.setAnimationLoop(self.animationLoop);
+        this.core.renderer.setAnimationLoop(this.animationLoop);
 
     }
 
     animationLoop = () => {
 
         this.stats.begin();
-        this.fpsControl.update(this.clock.getDelta());
+        // this.fpsControl.update(this.clock.getDelta());
+        this.orbitControls.update();
 
         // 用户移动或旋转才渲染，节省电量
         // if (this.fpsControl.isActive()) {
